5f8d4e2f886c3acf02a256c0afaa9a38a793d0cf
Mark10249 <hnguyen01248@csu.fullerton.edu>
2024-05-25 16:35:45 -0700

    changes to app, login, adn splash
    halp

---
+++ Fragrance-Max/app/(tabs)/splashscreen.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react';
+import { Animated, Image, StyleSheet, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+type RootStackParamList = {
+  login: undefined;
+  main: undefined;
+};
+
+const SplashScreen = () => {
+    const navigation = useNavigation<StackNavigationProp<RootStackParamList, 'main'>>();
+    const fadeAnim = useRef(new Animated.Value(0)).current;  // Initial value for opacity: 0
+  
+    useEffect(() => {
+      Animated.timing(
+        fadeAnim,
+        {
+          toValue: 1,
+          duration: 2000,
+          useNativeDriver: true,
+        }
+      ).start(() => {
+        navigation.navigate('main');
+      });
+    }, [navigation, fadeAnim]);
+  
+    return (
+      <View style={styles.container}>
+        <Animated.Image source={require('@/assets/images/Logo-fragrance.jpg')} style={{ ...styles.logo, opacity: fadeAnim }} />
+      </View>
+    );
+  };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+  },
+});
+
+export default SplashScreen;
--- Fragrance-Max/app/app.tsx
+++ Fragrance-Max/app/app.tsx
@@ -1,18 +1,28 @@
-import 'react-native-gesture-handler'; // Ensure this is at the top of your entry file
-import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginPage from '@/app/(tabs)/login'; // Ensure this path is correct
-import HomeScreen from '@/app/(tabs)/index'; // Ensure this path is correct
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import LoginPage from '@/app/(tabs)/login';
+import HomeScreen from '@/app/(tabs)/index';
+import SplashScreen from '@/app/(tabs)/splashscreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator initialRouteName="login">
+      <Tab.Screen name="login" component={LoginPage} />
+      <Tab.Screen name="index" component={HomeScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="login">
-        <Stack.Screen name="login" component={LoginPage} />
-        <Stack.Screen name="index" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="splashscreen">
+        <Stack.Screen name="splashscreen" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="main" component={TabNavigator} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
