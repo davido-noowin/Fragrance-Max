248511a146970e19bda2332398a3e8618a877825
David Nguyen <89553049+davido-noowin@users.noreply.github.com>
2024-05-25 20:22:06 -0700

    login logic using mongodb

--- backend/src/login.py
+++ backend/src/login.py
@@ -3,18 +3,33 @@ from fastapi.responses import JSONResponse
 from pymongo import MongoClient
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pydantic import BaseModel
+
 
 router = APIRouter()
 
-@router.get("/api/login")
-def login():
+class User(BaseModel):
+    email: str
+
+@router.post("/api/login")
+def login(username: User):
     url = "mongodb+srv://chienbinh456:Urgods_project123@cluster0.yvb700i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     client = MongoClient(url, server_api=ServerApi('1'))
+    result = None
 
     try:
-        client.admin.command('ping')
-        message = "Pinged your deployment. you successfully connected to MongoDB!"
+        db = client['Fragrances']
+        query = db.Email.find({
+            'email': username
+            }, {"email" : 1, "_id" : 0})
+
+        result_list = [doc for doc in query]
+
+        # Return JSON response
+        return JSONResponse(content=result_list)
+
     except Exception as e:
-        message = e
+        # Catch ConnectionFailure exceptions
+        message = f"Failed to connect to MongoDB: {e}"
 
     return JSONResponse(content=message)
