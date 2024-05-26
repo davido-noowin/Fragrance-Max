76587425ec21f09c1b9797501d7daeaa37e69ddb
Huan Nguyen <huannguyen2002@csu.fullerton.edu>
2024-05-25 18:03:29 -0700

    basic placeholder
    placeholder for reccomendation implementation.

---
+++ Fragrance-Max/app/(tabs)/reccomendationTab.tsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, ScrollView, Modal } from 'react-native';
+
+/*const RecommendationTab = () => {
+  const [recommendations, setRecommendations] = useState([]);
+  const [modalVisible, setModalVisible] = useState(true);
+
+  // Fetch recommendations from your MongoDB database
+  useEffect(() => {
+    // Replace this with your actual fetch function
+    fetchRecommendations().then(data => setRecommendations(data));
+  }, []);
+
+// Replace this with your actual fetch function
+const fetchRecommendations = async () => {
+  // Fetch data from your MongoDB database
+  // This is just a placeholder
+  return [
+    { image: 'blah blah', description: 'blah blah' },
+ 
+    // ...
+  ];
+};
+
+export default RecommendationTab;*/
