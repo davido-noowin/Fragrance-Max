0d953b3dc7a171cf6d7e2abf1c57db20e304e016
David Nguyen <davido.nguyen1212@gmail.com>
2024-07-01 21:18:59 -0700

    skeleton code for collection page

--- backend/src/fragranceCollection.py
+++ backend/src/fragranceCollection.py
@@ -1,40 +1,30 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from pymongo import MongoClient
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from pydantic import BaseModel
-
 
 router = APIRouter()
 
-class User(BaseModel):
-    email: str
 
-@router.post("/api/login")
-def login(username: User):
+@router.get("/api/get-collection")
+def getCollection(username: str):
+    '''
+    Returns the user's fragrance collection stored in mongoDB
+    '''
     url = None
     with open("mongoCluster.txt", 'r') as f:
         url = f.readline()
-    
+
     if not url:
         return JSONResponse(status_code=500, 
                             content="Could not connect to the mongo cluster.")
     client = MongoClient(url, server_api=ServerApi('1'))
 
     try:
-        db = client['Fragrances']
-        query = db.Email.find({
-            'email': username.email
-            }, {"email" : 1, "_id" : 0})
-
-        result_list = [doc for doc in query]
-
-        # Return JSON response
-        return JSONResponse(content=result_list)
-
+        # TODO: collection retrieval implementation
+        pass
     except Exception as e:
-        # Catch ConnectionFailure exceptions
         message = f"Failed to connect to MongoDB: {e}"
 
     return JSONResponse(status_code=500, content=message)
